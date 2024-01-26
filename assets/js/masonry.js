bc1dc4d853bbc0b320a191591f5df3d5356fa403
Andrew Boyer <asboyer@gmail.com>
2024-01-25 21:14:52 -0500

    improved project grid

--- assets/js/masonry.js
+++ assets/js/masonry.js
@@ -1,12 +1,13 @@
-$(document).ready(function () {
-  // Init Masonry
-  var $grid = $(".grid").masonry({
-    gutter: 10,
-    horizontalOrder: true,
-    itemSelector: ".grid-item",
-  });
-  // Layout Masonry after each image loads
-  $grid.imagesLoaded().progress(function () {
-    $grid.masonry("layout");
-  });
-});
+// $(document).ready(function () {
+//   // Init Masonry
+//   var $grid = $(".grid").masonry({
+//     isFitWidth: true,
+//     gutter: 10,
+//     horizontalOrder: true,
+//     itemSelector: ".grid-item",
+//   });
+//   // Layout Masonry after each image loads
+//   $grid.imagesLoaded().progress(function () {
+//     $grid.masonry("layout");
+//   });
+// });
